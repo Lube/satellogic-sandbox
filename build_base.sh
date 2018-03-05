9e302bd2196d5533dbaf6121bbf5e35b2cd837dd
Sebastian Luberriaga <sebastian.luberriaga@tiarg.com.ar>
2018-03-05 09:28:10 -0300

    fix py ver

--- build_base.sh
+++ build_base.sh
@@ -3,6 +3,6 @@
 export PYTHONPATH=${PWD}
 
 trap 'kill %1;' SIGINT
-{ python './src/bin/terran_base.py' & python './src/bin/web_server.py'; } 
+{ python3 './src/bin/terran_base.py' & python3 './src/bin/web_server.py'; } 
 
 
